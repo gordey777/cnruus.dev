b6998097fe478ae6f3b12c5102f6ef7d534cae8d
Oleg Gordiyenko <oleg.gordey777@gmail.com>
2016-09-07 12:27:27 +0300

    header and footer

--- wordpress/wp-content/themes/wp-framework/footer.php
+++ wordpress/wp-content/themes/wp-framework/footer.php
@@ -11,9 +11,172 @@
     </p><!-- /copyright -->
 
   </div><!-- /.inner -->
+  <div class="foot mt10 ">
+      <div class="font2 area">
+        <p>Адрес :410151，Чанша, Ваньцзяливэй 35,офис 311. &nbsp;&nbsp;&nbsp;&nbsp;Тел. +86 731 84028467&nbsp;&nbsp;&nbsp;&nbsp;РКБЦ：Российско-китайский бизнес-центр</p>
+        <p class="text-r">Сайт создан в:<a target="_blank" href="#">inemd.net</a></p>
+      </div>
+    </div>
+    <div id="leftsead">
+      <ul>
+        <li>
+          <a href="javascript:void(0)" class="youhui">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/l02.png" width="47" height="49" class="shows">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/a.png" width="57" height="49" class="hides">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/weixin.jpg" width="145" class="2wm" style="display: none; margin: -100px 57px 0 0">
+          </a>
+        </li>
+        <li>
+          <a href="http://wpa.qq.com/msgrd?v=3&amp;uin=282794290&amp;site=qq&amp;menu=yes" target="_blank">
+            <div class="hides" style="width: 161px; display: none;" id="qq">
+              <div class="hides" id="p1">
+                <img src="<?php echo get_template_directory_uri(); ?>/img/ll04.png">
+              </div>
+              <div class="hides" id="p2">
+                <span style="color: #FFF; font-size: 13px">282794290</span>
+              </div>
+            </div>
+            <img src="<?php echo get_template_directory_uri(); ?>/img/l04.png" width="47" height="49" class="shows">
+          </a>
+        </li>
+        <li id="tel">
+          <a href="javascript:void(0)">
+            <div class="hides" style="width: 161px; display: none;" id="tels">
+              <div class="hides" id="p1">
+                <img src="<?php echo get_template_directory_uri(); ?>/img/ll05.png">
+              </div>
+              <div class="hides" id="p4">
+                <span style="color: #FFF; font-size: 12px">+86 15388055177</span>
+              </div>
+            </div>
+            <img src="<?php echo get_template_directory_uri(); ?>/img/l05.png" width="47" height="49" class="shows">
+          </a>
+        </li>
+        <li id="mail">
+          <a href="javascript:void(0)">
+            <div class="hides" style="width: 161px; display: none;" id="mails">
+              <div class="hides" id="p1">
+                <img src="<?php echo get_template_directory_uri(); ?>/img/ll07.png" width="47" height="49">
+              </div>
+              <div class="hides" id="p3">
+                <span style="color: #FFF; font-size: 12px">hunanrussia@163.com</span>
+              </div>
+            </div>
+            <img src="<?php echo get_template_directory_uri(); ?>/img/l07.png" width="47" height="49" class="shows">
+          </a>
+        </li>
+        <li id="mail">
+          <a href="https://vk.com/cnruss" target="_blank">
+            <div class="hides" style="width: 161px; display: none;" id="mails">
+              <div class="hides" id="p1">
+                <img src="<?php echo get_template_directory_uri(); ?>/img/1109.png" width="47" height="49">
+              </div>
+              <div class="hides" id="p3" style=" background:#4f6f95;border-color:#4f6f95;">
+                <span style="color: #FFF; font-size: 12px">ВКонтакте</span>
+              </div>
+            </div>
+            <img src="<?php echo get_template_directory_uri(); ?>/img/109.png" width="47" height="49" class="shows">
+          </a>
+        </li>
+        <li id="btn">
+          <a id="top_btn">
+            <div class="hides" style="width: 161px; display: none">
+              <img src="<?php echo get_template_directory_uri(); ?>/img/ll08.png" width="161" height="49">
+            </div>
+            <img src="<?php echo get_template_directory_uri(); ?>/img/l08.png" width="47" height="49" class="shows">
+          </a>
+        </li>
+      </ul>
+    </div>
+
+
+
+
+    <script src="<?php echo get_template_directory_uri(); ?>/js/jquery-1.10.2.min.js" type="text/javascript"></script>
+    <script src="<?php echo get_template_directory_uri(); ?>/js/jquery.kinMaxShow-1.0.min.js" type="text/javascript"></script>
+    <script type="text/javascript">
+      $(function() {
+        $("#kinMaxShow").kinMaxShow();
+      });
+    </script>
+    <script type="text/javascript">
+      $(function() {
+        $("#AreaNav li:eq(0) a").addClass("cur");
+      });
+    </script>
+    <script>
+      var speed = 50
+      demo2.innerHTML = demo1.innerHTML
+
+      function Marquee() {
+        if (demo2.offsetTop - demo.scrollTop <= 0)
+          demo.scrollTop -= demo1.offsetHeight
+        else {
+          demo.scrollTop++
+        }
+      }
+      var MyMar = setInterval(Marquee, speed)
+      demo.onmouseover = function() {
+        clearInterval(MyMar)
+      }
+      demo.onmouseout = function() {
+        MyMar = setInterval(Marquee, speed)
+      }
+    </script>
+<!--     <script src="<?php echo get_template_directory_uri(); ?>/js/jquery.litenav.js" type="text/javascript"></script>
+    <script type="text/javascript">
+      $('#hotpic').liteNav(1000);
+    </script>-->
+    <script type="text/javascript">
+    function ShearcKey() {
+      var ShearText = $.trim($("#ShearText").val());
+      if (ShearText != "" && ShearText != "请输入您要搜索的关键词") {
+        location.href = "IntoRussia.aspx?str=" + ShearText;
+      } else {
+        alert("请输入关键字！");
+      }
+    }
+    $(document).ready(function() {
+      $("#leftsead a").hover(function() {
+        if ($(this).prop("className") == "youhui") {
+          $(this).children("<?php echo get_template_directory_uri(); ?>/img.hides").show();
+        } else {
+          $(this).children("div.hides").show();
+          $(this).children("<?php echo get_template_directory_uri(); ?>/img.shows").hide();
+          $(this).children("div.hides").animate({
+            marginRight: '0px'
+          }, '0');
+        }
+      }, function() {
+        if ($(this).prop("className") == "youhui") {
+          $(this).children("<?php echo get_template_directory_uri(); ?>/img.hides").hide();
+        } else {
+          $(this).children("div.hides").animate({
+            marginRight: '-163px'
+          }, 0, function() {
+            $(this).hide();
+            $(this).next("<?php echo get_template_directory_uri(); ?>/img.shows").show();
+          });
+        }
+      });
+      $("#top_btn").click(function() {
+        if (scroll == "off") return;
+        $("html,body").animate({
+          scrollTop: 0
+        }, 600);
+      });
+      //右侧导航 - 二维码
+      $(".youhui").mouseover(function() {
+        $(this).children(".2wm").show();
+      })
+      $(".youhui").mouseout(function() {
+        $(this).children(".2wm").hide();
+      });
+    });
+    </script>
 </footer><!-- /footer -->
 
     <?php wp_footer(); ?>
-
+  </form>
 </body>
 </html>
--- wordpress/wp-content/themes/wp-framework/header.php
+++ wordpress/wp-content/themes/wp-framework/header.php
@@ -19,6 +19,11 @@
   <![endif]-->
   <!-- css + javascript -->
   <?php wp_head(); ?>
+  <link href="<?php echo get_template_directory_uri(); ?>/css/reset.css" rel="stylesheet" type="text/css">
+  <link href="<?php echo get_template_directory_uri(); ?>/css/Common.css" rel="stylesheet" type="text/css">
+  <link href="<?php echo get_template_directory_uri(); ?>/css/Style.css" rel="stylesheet" type="text/css">
+  <link href="<?php echo get_template_directory_uri(); ?>/css/Index.css" rel="stylesheet" type="text/css">
+  <link href="<?php echo get_template_directory_uri(); ?>/css/top.css" rel="stylesheet" type="text/css">
 </head>
 <body <?php body_class(); ?>>
 <!-- wrapper -->
@@ -37,11 +42,71 @@
         <?php } ?>
       </div><!-- /logo -->
 
-      <nav class="nav" role="navigation">
-        <?php wpeHeadNav(); ?>
-      </nav><!-- /nav -->
-
     </div><!-- /.inner -->
+
+    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "#">
+<!-- saved from url=(0040)http://www.cnruss.com/Russian/index.aspx -->
+<html xmlns="#">
+
+
+  <form name="aspnetForm" method="post" action="#" id="aspnetForm">
+
+    <div class="head">
+      <div class="top">
+        <div class="area">
+          <h1 class="fl">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/logo.png">
+          </h1>
+          <div class="lang fr fw">
+            <a href="#">
+              <img src="<?php echo get_template_directory_uri(); ?>/img/cn.png">
+            </a>
+            <a href="#">
+              <img src="<?php echo get_template_directory_uri(); ?>/img/ru.png">
+            </a>
+          </div>
+        </div>
+        <div class="clear"></div>
+      </div>
+      <div class="menu">
+        <div class="area" id="AreaNav">
+                <?php wpeHeadNav(); ?>
+        </div>
+      </div>
+      <div class="clear"></div>
+    </div>
+    <div class="content">
+      <div class="search">
+        <div class="ss1 fl">
+          <input class="k2 lf" value="поиск по сайту:" onfocus="this.value=&#39;&#39;;" type="text" id="ShearText">
+          <a href="javascript:;" onclick="ShearcKey()">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/soso.gif" border="0"></a>
+        </div>
+        <div class="ss2 fr">
+          <a href="#" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/sun.jpg" width="113" height="41" border="0"></a>
+        </div>
+      </div>
+      <div class="clear"></div>
+      <!-- main slider -->
+      <div id="kinMaxShow" class="mb10">
+        <div>
+          <a href="#" target="_blank">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/201606281429579921.jpg" width="1020" height="270"></a>
+        </div>
+        <div>
+          <a href="#" target="_blank">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/201606281428282006.jpg" width="1020" height="270"></a>
+        </div>
+        <div>
+          <a href="#" target="_blank">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/20160628142249964.jpg" width="1020" height="270"></a>
+        </div>
+        <div>
+          <a href="#" target="_blank">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/201606281345452842.jpg" width="1020" height="270"></a>
+        </div>
+
+      </div>
   </header><!-- /header -->
 
   <section role="main">
